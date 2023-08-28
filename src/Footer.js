63dd39ee3f982cdbcb2a2dabb0737e86eec0ce04
SuadIb6 <suadibrahim1020@gmail.com>
2023-08-28 02:11:18 +0100

    Added axios integration so it shows the weather

---
+++ src/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+export default function Footer() {
+  return (
+    <small className="Footer">
+      <a
+        href="https://github.com/aubreyjustine713/weather-react-app"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open source code
+      </a>
+      <br /> by Suad Ibrahim
+    </small>
+  );
+}
